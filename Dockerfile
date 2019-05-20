1d0b8d82532b13d40301df45fbae5a02fbd9ffec
jackmo233 <mo2231031@live.cn>
2019-05-20 17:57:16 +0800

    优化脚本

--- Dockerfile
+++ Dockerfile
@@ -4,5 +4,4 @@ COPY build.sh /tmp/build.sh
 COPY sources.list.trusty /etc/apt/sources.list
 ADD src/ /src/
 RUN /bin/bash /tmp/build.sh
-USER opbuild
 CMD /src/opbuild.sh
--- src/opbuild.sh
+++ src/opbuild.sh
@@ -5,22 +5,22 @@ OWN=$(ls -ld /opt/opbuild |awk '{print $3}')
 cd $USERHOME
 
 if [ $OWN != opbuild ];then
-	sudo chown -R opbuild:opbuild /opt/opbuild
+	chown -R opbuild:opbuild /opt/opbuild
 fi
 
 
 case $1 in
 	pull)
-		bash -c /src/opupdate.sh
+		su - opbuild -c /src/opupdate.sh
 		;;
 	conf)
-		bash -c /src/opconfig.sh
+		su - opbuild -c /src/opconfig.sh
 		;;
 	make)
-		bash -c /src/opcompile.sh
+		su - opbuild -c /src/opcompile.sh
 		;;
 	dl)
-		bash -c /src/opdownload.sh
+		su - opbuild -c /src/opdownload.sh
 		;;
 	*)
 		echo "参数必须为 '|pull|conf|make|dl|'！"
