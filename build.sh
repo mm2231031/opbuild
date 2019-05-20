54c9841f40d8f6bd4b26ed71642f16698dd2e03d
jackmo233 <mo2231031@live.cn>
2019-05-20 16:38:51 +0800

    update

--- build.sh
+++ build.sh
@@ -3,6 +3,6 @@ mkdir /opt/opbuild
 useradd -d /opt/opbuild -s /bin/bash opbuild
 chown -R opbuild:opbuild /opt/opbuild
 apt-get update
-apt-get install --yes wget curl xz-utils libelf-dev build-essential asciidoc binutils bzip2 gawk gettext git libncurses5-dev libz-dev patch unzip zlib1g-dev lib32gcc1 libc6-dev-i386 subversion flex uglifyjs git-core gcc-multilib p7zip p7zip-full msmtp libssl-dev texinfo libglib2.0-dev xmlto qemu-utils upx libelf-dev autoconf automake libtool autopoint
+apt-get install --yes wget curl vim xz-utils libelf-dev build-essential asciidoc binutils bzip2 gawk gettext git libncurses5-dev libz-dev patch unzip zlib1g-dev lib32gcc1 libc6-dev-i386 subversion flex uglifyjs git-core gcc-multilib p7zip p7zip-full msmtp libssl-dev texinfo libglib2.0-dev xmlto qemu-utils upx libelf-dev autoconf automake libtool autopoint
 apt-get clean
 chmod +x /src/*.sh
