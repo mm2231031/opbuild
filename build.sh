63326d28cc121d03d2b78a1bceed48a38d2d3535
mm2231031 <mm2231031@gmail.com>
2022-02-11 16:11:22 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -2,6 +2,7 @@
 mkdir /opt/opbuild
 useradd -d /opt/opbuild -s /bin/bash opbuild
 chown -R opbuild:opbuild /opt/opbuild
+timedatectl set-timezone Asia/Shanghai
 apt-get update
 apt-get --yes install build-essential asciidoc binutils bzip2 gawk gettext git libncurses5-dev libz-dev patch python3 python2.7 unzip zlib1g-dev lib32gcc1 libc6-dev-i386 subversion flex uglifyjs git-core gcc-multilib p7zip p7zip-full msmtp libssl-dev texinfo libglib2.0-dev xmlto qemu-utils upx libelf-dev autoconf automake libtool autopoint device-tree-compiler g++-multilib antlr3 gperf wget curl swig rsync
 apt-get clean
